6f42c9efb50178d0185ea38b0c2000975c7c3c13
Юля Пономарева <julia@speakapp.com>
2017-11-12 12:19:35 +0300

    Initial Commit

---
+++ CalculatorBrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorBrain
+//
+//  Created by Юля Пономарева on 12.11.17.
+//  Copyright © 2017 Юля Пономарева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
